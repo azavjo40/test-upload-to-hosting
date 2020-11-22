566cea08b082bacb01904f1836e11f04660de8b9
azavjo40 <pabek92@gmail.com>
2020-11-22 01:39:40 +0100

    jahfakhdksjghfkdh

--- app.js
+++ app.js
@@ -14,9 +14,13 @@ app.use('/t', require('./routes/redirect.routes'))
 
 // тут даем фронтент если продакшн то что бы указать статичиский папку наш
 
+if (process.env.NODE_ENV === 'production') {
   app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || config.get('port') || 5000
 
